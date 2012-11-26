e876e72842f71acd4c61ad58468849cbbf6c8dda
Dave Pelletier <dpelletier@zynga.com>
2012-11-26 10:22:47 -0500

    Added seek bar for brightness of dots

--- src/com/github/efung/glimmer/GlimmerLiveWallpaper.java
+++ src/com/github/efung/glimmer/GlimmerLiveWallpaper.java
@@ -1,7 +1,5 @@
 package com.github.efung.glimmer;
 
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.handler.timer.ITimerCallback;
 import org.andengine.engine.handler.timer.TimerHandler;
@@ -30,8 +28,10 @@ import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.util.color.Color;
 import org.andengine.util.math.MathUtils;
 
-public class GlimmerLiveWallpaper extends BaseLiveWallpaperService implements SharedPreferences.OnSharedPreferenceChangeListener
-{
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class GlimmerLiveWallpaper extends BaseLiveWallpaperService implements SharedPreferences.OnSharedPreferenceChangeListener {
     private static final int CAMERA_WIDTH = 480;
     private static final int CAMERA_HEIGHT = 800;
 
@@ -45,22 +45,20 @@ public class GlimmerLiveWallpaper extends BaseLiveWallpaperService implements Sh
     // Prefs
     private int mMode;
     private float mColourChangePeriod;
+    private float mBrightness;
     private int mDotSize;
 
     private float mCurrentPitch; // rotation around X-axis, screen's horizontal axis (tilting forward and backward)
     private float mCurrentRoll; // rotation around Y-axis, screen's vertical axis (tilting left and right)
 
-
     @Override
-    public EngineOptions onCreateEngineOptions()
-    {
+    public EngineOptions onCreateEngineOptions() {
         this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
         return new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera);
     }
 
     @Override
-    public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws Exception
-    {
+    public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws Exception {
         PreferenceManager.getDefaultSharedPreferences(GlimmerLiveWallpaper.this).registerOnSharedPreferenceChangeListener(this);
 
         BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
@@ -70,38 +68,32 @@ public class GlimmerLiveWallpaper extends BaseLiveWallpaperService implements Sh
     }
 
     @Override
-    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws Exception
-    {
+    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws Exception {
         final Scene scene = new Scene();
         pOnCreateSceneCallback.onCreateSceneFinished(scene);
     }
 
     @Override
-    public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception
-    {
+    public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
         pOnPopulateSceneCallback.onPopulateSceneFinished();
     }
 
     @Override
-    public void onPauseGame()
-    {
+    public void onPauseGame() {
         super.onPauseGame();
 
         disableSensors();
     }
 
     @Override
-    public void onResumeGame()
-    {
+    public void onResumeGame() {
         final boolean settingsChanged = readSettingsFromPreferences();
 
-        if (mMode == GlimmerPreferenceActivity.PREFS_MODE_REFLECT_LIGHT)
-        {
+        if (mMode == GlimmerPreferenceActivity.PREFS_MODE_REFLECT_LIGHT) {
             enableSensors();
         }
 
-        if (settingsChanged)
-        {
+        if (settingsChanged) {
             resetScene();
             buildScene(this.mEngine.getScene());
         }
@@ -110,154 +102,136 @@ public class GlimmerLiveWallpaper extends BaseLiveWallpaperService implements Sh
     }
 
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences preference, String s)
-    {
+    public void onSharedPreferenceChanged(SharedPreferences preference, String s) {
         this.onPauseGame();
         this.onResumeGame();
     }
 
-    private boolean readSettingsFromPreferences()
-    {
+    private boolean readSettingsFromPreferences() {
         boolean settingsChanged = false;
         int temp;
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(GlimmerLiveWallpaper.this);
 
-        temp = Integer.valueOf(prefs.getString(this.getString(R.string.prefs_key_mode),
-                String.valueOf(GlimmerPreferenceActivity.PREFS_MODE_DEFAULT)));
-        if (this.mMode != temp)
-        {
+        temp = Integer.valueOf(prefs.getString(this.getString(R.string.prefs_key_mode), String.valueOf(GlimmerPreferenceActivity.PREFS_MODE_DEFAULT)));
+        if (this.mMode != temp) {
             this.mMode = temp;
             settingsChanged = true;
         }
 
-        temp = Integer.valueOf(prefs.getString(this.getString(R.string.prefs_key_dot_size),
-                String.valueOf(GlimmerPreferenceActivity.PREFS_DOT_SIZE_DEFAULT)));
-        if (this.mDotSize != temp)
-        {
+        temp = Integer.valueOf(prefs.getString(this.getString(R.string.prefs_key_dot_size), String.valueOf(GlimmerPreferenceActivity.PREFS_DOT_SIZE_DEFAULT)));
+        if (this.mDotSize != temp) {
             this.mDotSize = temp;
             settingsChanged = true;
         }
 
         temp = prefs.getInt(this.getString(R.string.prefs_key_colour_change_period), 6);
-        if (this.mColourChangePeriod != temp)
-        {
+        if (this.mColourChangePeriod != temp) {
             this.mColourChangePeriod = temp;
+            settingsChanged = true;
+        }
+
+        temp = prefs.getInt(this.getString(R.string.prefs_key_brightness), 50);
+        if (this.mBrightness != temp) {
+            this.mBrightness = temp;
             settingsChanged = true;
         }
 
         return settingsChanged;
     }
 
-    private String getParticleFilename(final int prefsDotSize)
-    {
-        switch (prefsDotSize)
-        {
-            case GlimmerPreferenceActivity.PREFS_DOT_SIZE_L:
+    private String getParticleFilename(final int prefsDotSize) {
+        switch (prefsDotSize) {
+            case GlimmerPreferenceActivity.PREFS_DOT_SIZE_L :
                 return "nexusdot_l.png";
-            case GlimmerPreferenceActivity.PREFS_DOT_SIZE_M:
-            default:
+            case GlimmerPreferenceActivity.PREFS_DOT_SIZE_M :
+            default :
                 return "nexusdot_m.png";
-            case GlimmerPreferenceActivity.PREFS_DOT_SIZE_S:
+            case GlimmerPreferenceActivity.PREFS_DOT_SIZE_S :
                 return "nexusdot_s.png";
         }
     }
 
-    private void loadParticleImage()
-    {
-        this.mParticleTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mBitmapTextureAtlas,
-                this, getParticleFilename(this.mDotSize), 0, 0);
+    private void loadParticleImage() {
+        this.mParticleTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mBitmapTextureAtlas, this,
+                getParticleFilename(this.mDotSize), 0, 0);
         this.mBitmapTextureAtlas.load();
     }
 
-    private void unloadParticleImage()
-    {
+    private void unloadParticleImage() {
         this.mBitmapTextureAtlas.unload();
         this.mParticleTextureRegion = null;
     }
 
-    private void enableSensors()
-    {
-        this.mEngine.enableOrientationSensor(this, new IOrientationListener()
-        {
+    private void enableSensors() {
+        this.mEngine.enableOrientationSensor(this, new IOrientationListener() {
             @Override
-            public void onOrientationChanged(OrientationData pOrientationData)
-            {
+            public void onOrientationChanged(OrientationData pOrientationData) {
                 GlimmerLiveWallpaper.this.mCurrentPitch = pOrientationData.getPitch();
                 GlimmerLiveWallpaper.this.mCurrentRoll = pOrientationData.getRoll();
             }
 
             @Override
-            public void onOrientationAccuracyChanged(OrientationData pOrientationData)
-            {
+            public void onOrientationAccuracyChanged(OrientationData pOrientationData) {
             }
-        },
-                new OrientationSensorOptions(SensorDelay.GAME) // NORMAL makes it stutter, but GAME uses more battery...
-        );
+        }, new OrientationSensorOptions(SensorDelay.GAME) // NORMAL makes it stutter, but GAME uses more battery...
+                );
     }
 
-    private void disableSensors()
-    {
+    private void disableSensors() {
         this.mEngine.disableOrientationSensor(this);
     }
 
-    private Color getRandomColor()
-    {
+    private Color getRandomColor() {
         return new Color(MathUtils.RANDOM.nextInt(255) / 255f, MathUtils.RANDOM.nextInt(255) / 255f, MathUtils.RANDOM.nextInt(255) / 255f);
     }
 
-    private void resetScene()
-    {
+    private void resetScene() {
         unloadParticleImage();
         this.mEngine.getScene().detachChildren();
         this.mEngine.clearUpdateHandlers();
     }
 
-    private void buildScene(final Scene scene)
-    {
+    private void buildScene(final Scene scene) {
         loadParticleImage();
 
-        switch (mMode)
-        {
-            case GlimmerPreferenceActivity.PREFS_MODE_CHANGE_COLOUR:
-            default:
+        switch (mMode) {
+            case GlimmerPreferenceActivity.PREFS_MODE_CHANGE_COLOUR :
+            default :
                 buildColourChangeScene(scene);
                 break;
-            case GlimmerPreferenceActivity.PREFS_MODE_REFLECT_LIGHT:
+            case GlimmerPreferenceActivity.PREFS_MODE_REFLECT_LIGHT :
                 buildReflectLightScene(scene);
                 break;
         }
     }
 
-    private void buildReflectLightScene(final Scene scene)
-    {
-        final GridParticleEmitter particleEmitter = new GridParticleEmitter(CAMERA_WIDTH * 0.5f,  CAMERA_HEIGHT * 0.5f, CAMERA_WIDTH, CAMERA_HEIGHT,
+    private void buildReflectLightScene(final Scene scene) {
+        final GridParticleEmitter particleEmitter = new GridParticleEmitter(CAMERA_WIDTH * 0.5f, CAMERA_HEIGHT * 0.5f, CAMERA_WIDTH, CAMERA_HEIGHT,
                 this.mParticleTextureRegion.getWidth(), this.mParticleTextureRegion.getHeight(), false);
         final int maxParticles = particleEmitter.getGridTilesX() * particleEmitter.getGridTilesY();
-        this.mParticleSystem = new BatchedSpriteParticleSystem(particleEmitter, maxParticles, maxParticles, maxParticles + maxParticles/4 /* A little extra to ensure coverage */,
+        this.mParticleSystem = new BatchedSpriteParticleSystem(particleEmitter, maxParticles, maxParticles, maxParticles + maxParticles / 4 /*
+                                                                                                                                             * A little extra to
+                                                                                                                                             * ensure coverage
+                                                                                                                                             */,
                 this.mParticleTextureRegion, this.getVertexBufferObjectManager());
 
         this.mParticleSystem.addParticleInitializer(new ColorParticleInitializer<UncoloredSprite>(getRandomColor()));
         this.mParticleSystem.addParticleInitializer(new RotationParticleInitializer<UncoloredSprite>(-90f, 90f));
 
-        this.mParticleSystem.addParticleModifier(new IParticleModifier<UncoloredSprite>()
-        {
+        this.mParticleSystem.addParticleModifier(new IParticleModifier<UncoloredSprite>() {
             @Override
-            public void onUpdateParticle(Particle<UncoloredSprite> pParticle)
-            {
+            public void onUpdateParticle(Particle<UncoloredSprite> pParticle) {
                 UncoloredSprite sprite = pParticle.getEntity();
                 sprite.setAlpha(getAlphaFromRotation(sprite.getRotation(), GlimmerLiveWallpaper.this.mCurrentPitch + GlimmerLiveWallpaper.this.mCurrentRoll));
             }
 
             @Override
-            public void onInitializeParticle(Particle<UncoloredSprite> pParticle)
-            {
+            public void onInitializeParticle(Particle<UncoloredSprite> pParticle) {
                 UncoloredSprite sprite = pParticle.getEntity();
                 sprite.setAlpha(getAlphaFromRotation(sprite.getRotation(), 0));
             }
 
-
-            private float getAlphaFromRotation(final float pParticleRotation, float pCombinedTilt)
-            {
+            private float getAlphaFromRotation(final float pParticleRotation, float pCombinedTilt) {
                 // The closer the particle angle is to the tilt angle, the brighter it is.
                 // Can't really see screen if tilt is more than 45 degrees, so we clamp it.
                 // Maximum difference will be 180 degrees, so normalize then convert to alpha.
@@ -269,13 +243,12 @@ public class GlimmerLiveWallpaper extends BaseLiveWallpaperService implements Sh
         scene.attachChild(this.mParticleSystem);
     }
 
-    private void buildColourChangeScene(final Scene scene)
-    {
-        final GridParticleEmitter particleEmitter = new GridParticleEmitter(CAMERA_WIDTH * 0.5f,  CAMERA_HEIGHT * 0.5f, CAMERA_WIDTH, CAMERA_HEIGHT,
+    private void buildColourChangeScene(final Scene scene) {
+        final GridParticleEmitter particleEmitter = new GridParticleEmitter(CAMERA_WIDTH * 0.5f, CAMERA_HEIGHT * 0.5f, CAMERA_WIDTH, CAMERA_HEIGHT,
                 this.mParticleTextureRegion.getWidth(), this.mParticleTextureRegion.getHeight(), false);
         final int maxParticles = particleEmitter.getGridTilesX() * particleEmitter.getGridTilesY();
-        this.mParticleSystem = new BatchedSpriteParticleSystem(particleEmitter, maxParticles / PARTICLE_LIFETIME, maxParticles / PARTICLE_LIFETIME, maxParticles,
-                this.mParticleTextureRegion, this.getVertexBufferObjectManager());
+        this.mParticleSystem = new BatchedSpriteParticleSystem(particleEmitter, maxParticles / PARTICLE_LIFETIME, maxParticles / PARTICLE_LIFETIME,
+                maxParticles, this.mParticleTextureRegion, this.getVertexBufferObjectManager());
 
         Color initialColor = getRandomColor();
         MutableColorParticleInitializer<UncoloredSprite> colorParticleInitializer = new MutableColorParticleInitializer<UncoloredSprite>(initialColor);
@@ -286,42 +259,35 @@ public class GlimmerLiveWallpaper extends BaseLiveWallpaperService implements Sh
         this.mParticleSystem.addParticleModifier(new AlphaParticleModifier<UncoloredSprite>(0, PARTICLE_LIFETIME / 2f, 0.3f, 1f));
         this.mParticleSystem.addParticleModifier(new AlphaParticleModifier<UncoloredSprite>(PARTICLE_LIFETIME / 2f, PARTICLE_LIFETIME, 1f, 0.3f));
 
-        this.mEngine.registerUpdateHandler(new TimerHandler(mColourChangePeriod, true,
-                new ChangingColorParticleInitializerTimerHandler(this.mParticleSystem, initialColor,
-                        colorParticleInitializer)));
+        this.mEngine.registerUpdateHandler(new TimerHandler(mColourChangePeriod, true, new ChangingColorParticleInitializerTimerHandler(initialColor,
+                colorParticleInitializer)));
 
         scene.attachChild(this.mParticleSystem);
     }
 
-    private class ChangingColorParticleInitializerTimerHandler implements ITimerCallback
-    {
+    private class ChangingColorParticleInitializerTimerHandler implements ITimerCallback {
         private int mCurrentParticleColorARGB;
         private float hsv[] = new float[3];
         private MutableColorParticleInitializer<UncoloredSprite> mCurrentParticleInitializer;
-        private BatchedSpriteParticleSystem mParticleSystem;
 
-        public ChangingColorParticleInitializerTimerHandler(final BatchedSpriteParticleSystem pParticleSystem, final Color pColor, final MutableColorParticleInitializer<UncoloredSprite> pInitializer)
-        {
-            this.mParticleSystem = pParticleSystem;
+        public ChangingColorParticleInitializerTimerHandler(final Color pColor, final MutableColorParticleInitializer<UncoloredSprite> pInitializer) {
             this.mCurrentParticleInitializer = pInitializer;
             this.mCurrentParticleColorARGB = pColor.getARGBPackedInt();
         }
 
         @Override
-        public void onTimePassed(TimerHandler pTimerHandler)
-        {
+        public void onTimePassed(TimerHandler pTimerHandler) {
             android.graphics.Color.colorToHSV(this.mCurrentParticleColorARGB, hsv);
 
             // Perturb values by hue, avoiding overly dark or bright colours
             hsv[0] += MathUtils.randomSign() * (MathUtils.RANDOM.nextFloat() * 20.0f + 10.0f); // between 10 and 30 degrees
             if (hsv[0] > 360f) {
                 hsv[0] -= 360f;
-            }
-            else if (hsv[0] < 0f) {
+            } else if (hsv[0] < 0f) {
                 hsv[0] += 360f;
             }
             hsv[1] = MathUtils.bringToBounds(0.3f, 0.7f, hsv[1] + MathUtils.randomSign() * 0.1f);
-            hsv[2] = MathUtils.bringToBounds(0.3f, 0.7f, hsv[2] + MathUtils.randomSign() * 0.1f);
+            hsv[2] = ((float) mBrightness) / 100.0f;
 
             this.mCurrentParticleColorARGB = android.graphics.Color.HSVToColor(hsv);
 
--- src/com/github/efung/glimmer/GlimmerPreferenceActivity.java
+++ src/com/github/efung/glimmer/GlimmerPreferenceActivity.java
@@ -8,8 +8,7 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
-public class GlimmerPreferenceActivity extends PreferenceActivity
-{
+public class GlimmerPreferenceActivity extends PreferenceActivity {
     public static final int PREFS_DOT_SIZE_S = 0;
     public static final int PREFS_DOT_SIZE_M = 1;
     public static final int PREFS_DOT_SIZE_L = 2;
@@ -24,33 +23,26 @@ public class GlimmerPreferenceActivity extends PreferenceActivity
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
 
-        ListPreference modePref = (ListPreference)findPreference(this.getString(R.string.prefs_key_mode));
+        ListPreference modePref = (ListPreference) findPreference(this.getString(R.string.prefs_key_mode));
 
         final SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null && sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null)
-        {
-            modePref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener()
-            {
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null && sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
+            modePref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
                 @Override
-                public boolean onPreferenceChange(Preference preference, Object o)
-                {
+                public boolean onPreferenceChange(Preference preference, Object o) {
                     return handleModePreferenceChange((ListPreference) preference, (String) o);
                 }
             });
-        }
-        else
-        {
+        } else {
             modePref.setValue(this.getResources().getStringArray(R.array.modeValues)[PREFS_MODE_DEFAULT]);
             this.getPreferenceScreen().removePreference(modePref);
         }
 
-        ListPreference dotSizePref = (ListPreference)findPreference(this.getString(R.string.prefs_key_dot_size));
-        dotSizePref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener()
-        {
+        ListPreference dotSizePref = (ListPreference) findPreference(this.getString(R.string.prefs_key_dot_size));
+        dotSizePref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
-            public boolean onPreferenceChange(Preference preference, Object o)
-            {
-                return handleDotSizePreferenceChange((ListPreference) preference, (String)o);
+            public boolean onPreferenceChange(Preference preference, Object o) {
+                return handleDotSizePreferenceChange((ListPreference) preference, (String) o);
             }
         });
 
@@ -58,18 +50,20 @@ public class GlimmerPreferenceActivity extends PreferenceActivity
         handleDotSizePreferenceChange(dotSizePref, dotSizePref.getValue());
     }
 
-    private boolean handleModePreferenceChange(final ListPreference modePref, final String newValue)
-    {
+    private boolean handleModePreferenceChange(final ListPreference modePref, final String newValue) {
         modePref.setValue(newValue);
         modePref.setTitle(this.getString(R.string.prefs_mode_title) + ": " + modePref.getEntry());
 
-        SeekBarPreference seekBarPref = (SeekBarPreference)findPreference(this.getString(R.string.prefs_key_colour_change_period));
+        SeekBarPreference seekBarPref = (SeekBarPreference) findPreference(this.getString(R.string.prefs_key_colour_change_period));
         seekBarPref.setEnabled(newValue.equals(this.getResources().getStringArray(R.array.modeValues)[PREFS_MODE_CHANGE_COLOUR]));
+
+        seekBarPref = (SeekBarPreference) findPreference(this.getString(R.string.prefs_key_brightness));
+        seekBarPref.setEnabled(newValue.equals(this.getResources().getStringArray(R.array.modeValues)[PREFS_MODE_CHANGE_COLOUR]));
+
         return true;
     }
 
-    private boolean handleDotSizePreferenceChange(final ListPreference dotSizePref, final String newValue)
-    {
+    private boolean handleDotSizePreferenceChange(final ListPreference dotSizePref, final String newValue) {
         dotSizePref.setValue(newValue);
         dotSizePref.setTitle(this.getString(R.string.prefs_dot_size_title) + ": " + dotSizePref.getEntry());
         return true;
