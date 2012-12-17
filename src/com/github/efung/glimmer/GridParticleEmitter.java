8c1cf144e2e485cab9038a27952e096a119131ee
Eric Fung <efung@acm.org>
2012-12-16 22:13:37 -0500

    Convert unnecessary instance variables to locals

--- src/com/github/efung/glimmer/GridParticleEmitter.java
+++ src/com/github/efung/glimmer/GridParticleEmitter.java
@@ -12,10 +12,6 @@ import static org.andengine.util.Constants.VERTEX_INDEX_Y;
 
 public class GridParticleEmitter extends BaseRectangleParticleEmitter
 {
-    protected float mGridWidth;
-    protected float mGridHeight;
-    protected int mGridTilesX;
-    protected int mGridTilesY;
     protected int mGridTiles;
     private boolean mRandomFill = true;
     private float mGridCoordsX[];
@@ -29,24 +25,22 @@ public class GridParticleEmitter extends BaseRectangleParticleEmitter
 
     public GridParticleEmitter(final float pCenterX, final float pCenterY, final float pWidth, final float pHeight, final float pGridWidth, final float pGridHeight, final boolean pRandomFill) {
         super(pCenterX, pCenterY, pWidth, pHeight);
-        this.mGridWidth = pGridWidth;
-        this.mGridHeight = pGridHeight;
-        this.mGridTilesX = Math.round(FloatMath.floor(pWidth / pGridWidth));
-        this.mGridTilesY = Math.round(FloatMath.floor(pHeight / pGridHeight));
-        this.mGridTiles = this.mGridTilesX * this.mGridTilesY;
+        final int gridTilesX = Math.round(FloatMath.floor(pWidth / pGridWidth));
+        final int gridTilesY = Math.round(FloatMath.floor(pHeight / pGridHeight));
+        this.mGridTiles = gridTilesX * gridTilesY;
         this.mRandomFill = pRandomFill;
 
         this.mGridCoordsX = new float[this.mGridTiles];
         this.mGridCoordsY = new float[this.mGridTiles];
         this.mIndices = new Integer[this.mGridTiles];
 
-        for (int i = 0; i < mGridTilesX; i++)
+        for (int i = 0; i < gridTilesX; i++)
         {
-            for (int j = 0; j < mGridTilesY; j++)
+            for (int j = 0; j < gridTilesY; j++)
             {
-                this.mGridCoordsX[this.mGridTilesY * i + j] = this.mCenterX - this.mWidthHalf + i * this.mGridWidth;
-                this.mGridCoordsY[this.mGridTilesY * i + j] = this.mCenterY - this.mHeightHalf + j * this.mGridHeight;
-                this.mIndices[this.mGridTilesY * i + j] = this.mGridTilesY * i + j;
+                this.mGridCoordsX[gridTilesY * i + j] = this.mCenterX - this.mWidthHalf + i * pGridWidth;
+                this.mGridCoordsY[gridTilesY * i + j] = this.mCenterY - this.mHeightHalf + j * pGridHeight;
+                this.mIndices[gridTilesY * i + j] = gridTilesY * i + j;
             }
         }
 
